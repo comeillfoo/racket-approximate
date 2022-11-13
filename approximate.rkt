5933f77a40eb6d2a3a36e1f5848567fcd419edc4
comeillfoo <rayveleaveme34@inbox.ru>
2022-11-13 11:53:15 +0000

    feat: main methods from 3rd lab of calculation math

--- approximate.rkt
+++ approximate.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require math/array)
+(require math/matrix)
 
 (define (read-csv-to-strings)
   (for/list ([xy (in-lines (current-input-port))])
@@ -10,4 +12,61 @@
   (map (lambda (xy) (cons (string->number (first xy)) (string->number (second xy)))) csv))
 
 
-(csv-function-tabulate (read-csv-to-strings))
+(define (linear-coefficients N SX SXX SY SXY)
+  (let*
+    ([delta (lambda (arg1 arg2 arg3 arg4) (- (* arg1 arg2) (* arg3 arg4)))]
+     [delta0 (delta SXX N SX SX)]
+     [delta1 (delta SXY N SX SY)]
+     [delta2 (delta SXX SY SX SXY)]
+     [a (/ delta1 delta0)]
+     [b (/ delta2 delta0)])
+    (cons a b)))
+
+
+(define (linear N SX SXX SY SXY)
+  (let*
+    ([coefficients (linear-coefficients N SX SXX SY SXY)]
+     [a (first coefficients)]
+     [b (second coefficients)])
+    (lambda (x) (+ (* a x) b))))
+
+
+(define (quadratic N SX SXX SY SXY SXXX SXXXX SXXY)
+  (let*
+    ([coefficients
+      (array->list
+        (matrix-solve
+          (matrix [[N SX SXX] [SX SXX SXXX] [SXX SXXX SXXXX]])
+          (col-matrix [SY SXY SXXY])))]
+     [a0 (first coefficients)]
+     [a1 (second coefficients)]
+     [a2 (third coefficients)])
+    (lambda (x) (+ a0 (* a1 x) (* a2 x x)))))
+
+
+(define (exponential N SX SXX SLnY SXLnY)
+  (let*
+    ([coefficients (linear-coefficients N SX SXX SLnY SXLnY)]
+     [A (first coefficients)]
+     [B (second coefficients)]
+     [a (exp B)]
+     [b A])
+    (lambda (x) (* a (exp (* b x))))))
+
+
+(define (logarithmic N SLnX SLnX2 SY SLnXY)
+  (let*
+    ([coefficients (linear-coefficients N SLnX SLnX2 SY SLnXY)]
+     [a (first coefficients)]
+     [b (second coefficients)])
+    (lambda (x) (+ (* a (log x)) b))))
+
+
+(define (power N SLnX SLnX2 SLnY SLnXLnY)
+  (let*
+    ([coefficients (linear-coefficients N SLnX SLnX2 SLnY SLnXLnY)]
+     [A (first coefficients)]
+     [B (second coefficients)]
+     [a (exp B)]
+     [b A])
+    (lambda (x) (* a (expt x b)))))
